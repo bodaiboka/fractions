a53562f052ee14227040174fe99bc81809fca190
boka <bodaiboka@gmail.com>
2023-06-21 11:16:52 +0200

    Fix some typos.

--- libFractional/fraction.cpp
+++ libFractional/fraction.cpp
@@ -1,26 +1,26 @@
-#include "include/fraction.h"
+#include "fraction.h"
 #include <stdexcept>
 #include <sstream>
 
-void Fraction::init(int whole, int nominator, int denominator)
+void Fraction::init(int whole, int numerator, int denominator)
 {
-	m_nominator = abs(nominator) + abs(whole) * abs(denominator);
+	m_numerator = abs(numerator) + abs(whole) * abs(denominator);
 	m_denominator = denominator;
-	m_nominator *= (whole < 0 || nominator < 0) ? -1 : 1;
+	m_numerator *= (whole < 0 || numerator < 0) ? -1 : 1;
 	if (m_denominator == 0)
 	{
 		throw runtime_error("Division by zero is not allowed.");
 	}
 }
 
-Fraction::Fraction(int whole, int nominator, int denominator)
+Fraction::Fraction(int whole, int numerator, int denominator)
 {
-	init(whole, nominator, denominator);
+	init(whole, numerator, denominator);
 }
 
 Fraction::Fraction(string expression)
 {
-	int whole, nominator, denominator;
+	int whole, numerator, denominator;
 	size_t andPos = expression.find('&');
 	size_t slashPos = expression.find('/');
 
@@ -29,7 +29,7 @@ Fraction::Fraction(string expression)
 		whole = stoi(expression.substr(0, andPos));
 		if (slashPos != string::npos)
 		{
-			nominator = stoi(expression.substr(andPos + 1, slashPos - andPos +1));
+			numerator = stoi(expression.substr(andPos + 1, slashPos - andPos +1));
 			denominator = stoi(expression.substr(slashPos+1));
 		}
 		else
@@ -42,60 +42,60 @@ Fraction::Fraction(string expression)
 		if (slashPos != string::npos)
 		{
 			whole = 0;
-			nominator = stoi(expression.substr(0, slashPos));
+			numerator = stoi(expression.substr(0, slashPos));
 			denominator = stoi(expression.substr(slashPos+1));
 		}
 		else
 		{
 			whole = stoi(expression);
-			nominator = 0;
-			denominator = 1; // only technical representation: since nominator is 0 the fraction itself is also 0
+			numerator = 0;
+			denominator = 1; // only technical representation: since numerator is 0 the fraction itself is also 0
 		}
 	}
-	if ((denominator < 0) || (whole != 0 && (nominator < 0 || denominator < 0)))
+	if ((denominator < 0) || (whole != 0 && (numerator < 0 || denominator < 0)))
 	{
 		throw runtime_error("A negative sign is detected in an incorrect position. Negative signs are allowed only to the left of operands. Please check the expression and ensure that any negative sign is placed immediately before its associated operand.");
 	}
-	init(whole, nominator, denominator);
+	init(whole, numerator, denominator);
 }
 
 string Fraction::toString()
 {
-	int whole, nominator, gc;
+	int whole, numerator, gc;
 	whole = 0;
-	gc = gcd(abs(m_nominator), abs(m_denominator));
-	m_nominator /= gc;
+	gc = gcd(abs(m_numerator), abs(m_denominator));
+	m_numerator /= gc;
 	m_denominator /= gc;
-	nominator = abs(m_nominator);
-	if (abs(m_nominator) >= abs(m_denominator))
+	numerator = abs(m_numerator);
+	if (abs(m_numerator) >= abs(m_denominator))
 	{
-		whole += abs(m_nominator) / abs(m_denominator);
-		nominator = abs(m_nominator) % abs(m_denominator);
+		whole += abs(m_numerator) / abs(m_denominator);
+		numerator = abs(m_numerator) % abs(m_denominator);
 	}
 	ostringstream oss;
-	oss << ((m_nominator < 0 && m_denominator > 0) || (m_nominator > 0 && m_denominator < 0) ? "-" : "");
+	oss << ((m_numerator < 0 && m_denominator > 0) || (m_numerator > 0 && m_denominator < 0) ? "-" : "");
 	oss << (whole != 0 ? to_string(whole) : "");
-	oss << (whole != 0 && nominator != 0 ? "&" : "");
-	oss << (nominator != 0 ? to_string(abs(nominator)) + "/" + to_string(m_denominator) : "");
-	oss << (whole == 0 && nominator == 0 ? "0" : "");
+	oss << (whole != 0 && numerator != 0 ? "&" : "");
+	oss << (numerator != 0 ? to_string(abs(numerator)) + "/" + to_string(m_denominator) : "");
+	oss << (whole == 0 && numerator == 0 ? "0" : "");
 	return oss.str();
 }
 
 Fraction Fraction::operator+(const Fraction& fraction) const
 {
-	return Fraction(0, m_nominator * fraction.denominator() + m_denominator * fraction.nominator(), m_denominator * fraction.denominator());
+	return Fraction(0, m_numerator * fraction.denominator() + m_denominator * fraction.numerator(), m_denominator * fraction.denominator());
 }
 Fraction Fraction::operator-(const Fraction& fraction) const
 {
-	return Fraction(0, m_nominator * fraction.denominator() - m_denominator * fraction.nominator(), m_denominator * fraction.denominator());
+	return Fraction(0, m_numerator * fraction.denominator() - m_denominator * fraction.numerator(), m_denominator * fraction.denominator());
 }
 Fraction Fraction::operator*(const Fraction& fraction) const
 {
-	return Fraction(0, m_nominator * fraction.nominator(), m_denominator * fraction.denominator());
+	return Fraction(0, m_numerator * fraction.numerator(), m_denominator * fraction.denominator());
 }
 Fraction Fraction::operator/(const Fraction& fraction) const
 {
-	return Fraction(0, m_nominator * fraction.denominator(), m_denominator * fraction.nominator());
+	return Fraction(0, m_numerator * fraction.denominator(), m_denominator * fraction.numerator());
 }
 
 int Fraction::gcd(int a, int b)
--- libFractional/include/fraction.h
+++ libFractional/include/fraction.h
@@ -7,16 +7,16 @@ using namespace std;
 class Fraction {
 
 private:
-	int m_nominator;
+	int m_numerator;
 	int m_denominator;
 	int gcd(int a, int b); // to find the greatest common divider
 
 public:
-	Fraction(int whole = 0, int nominator = 0, int denominator = 1);
-	Fraction(string); // we can create Fraction by a given string like: "whole&nominator/denominator" or "whole" or "nominator/denominator"
+	Fraction(int whole = 0, int numerator = 0, int denominator = 1);
+	Fraction(string); // we can create Fraction by a given string like: "whole&numerator/denominator" or "whole" or "numerator/denominator"
 	void init(int, int, int);
 	string toString();
-	inline int nominator() const { return m_nominator; }
+	inline int numerator() const { return m_numerator; }
 	inline int denominator() const { return m_denominator; }
 	Fraction operator+(const Fraction& fraction) const;
 	Fraction operator-(const Fraction& fraction) const;
